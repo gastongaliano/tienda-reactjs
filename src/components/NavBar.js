23077d67130703f1c6277a9ace398fd7df2c7144
Gaston Galiano <grgaliano91@gmail.com>
2020-12-09 18:31:36 -0300

    segundo push

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function Navigationbar() {
+    return
+        <NavBar>
+            <h1>TiendArt Argentina</h1>
+            <h3>Nuevos ingresos 2021</h3>
+            <p>Obra 1 - $3.000</p>
+            <p>Obra 2 - $4.000</p>
+            <p>Obra 3 - $10.000</p>
+            <p>Obra 4 - $20.000</p>
+            <p>Obra 5 - $40.000</p>          
+        </NavBar>
+}
